285fd2ecff7d78c651d04f12fd7922deb6104a12
vincascalvii <vincascalvii95@gmail.com>
2022-01-12 23:14:33 +1100

    Test WP rest api

--- js/main.js
+++ js/main.js
@@ -20,35 +20,53 @@ fetch(fetchURL)
 // Handle the data
 .then(function (data) {
 
+	// Get the container and all the chapters
 	var chapters = data['posts'];
 	var container = document.querySelector('#chapter-list');
 
+	// Loop through the list of chapters
 	for ( var i = 0; i < chapters.length; i++ ) {
 
+		// Get the chapter number and name
 		var cNumber = chapters[i]['title'].split(' ')[0];
 		var cName = chapters[i]['title'].split(' ')[1];
 
+		// Build the chapter number element
 		var number = document.createElement('span');
  			number.classList.add('chapter-number');
  			number.innerHTML = cNumber;
 
+ 		// Build the chapter name element
  		var name = document.createElement('span');
  			name.classList.add('chapter-name');
  			name.innerHTML = cName;
 
- 		var prevID = chapters[i - 1]['ID'] || '0';
- 		var nextID = chapters[i + 1]['ID'] || '0';
+ 		// Check if previous chapter exists
+ 		if ( typeof chapters[i - 1]['ID'] !== 'undefined' )
+ 			var prevID = chapters[i - 1]['ID'];
+ 		else
+ 			var prevID = '0';
 
+ 		// Check if next chapter exists
+ 		if ( typeof chapters[i + 1]['ID'] !== 'undefined' )
+ 			var prevID = chapters[i + 1]['ID'];
+ 		else
+ 			var prevID = '0';
+
+ 		// Build the chapter link element with both number and name
  		var link = document.createElement('a');
 			link.classList.add('chapter-link');
 			link.href = '/freedom/chapter?id=' + chapters[i]['ID'] + '&prev=' + prevID + '&next=' + nextID;
 			link.setAttribute('aria-label', 'Go to');
+			link.appendChild(number);
 			link.appendChild(name);
 
+		// Build the list item element with chapter link
 		var item = document.createElement('li');
 			item.classList.add('chapter-item');
 			item.appendChild(link);
 
+		// Add the chapter element to the list / container
 		container.appendChild(item);
 	}
 })
